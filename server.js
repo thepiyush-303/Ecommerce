6aefc9431949d214729eb2fe71a28f85c7683e28
Piyush Bhatt <bhattpiyush@gmail.com>
2024-04-15 21:34:28 +0530

    Second Commit

--- server.js
+++ server.js
@@ -1,51 +1,56 @@
 // this will be the starting file of js
-const express = require("express")
-const mongoose = require("mongoose")
-const app = express()
-const server_config = require("./config/serverConfig")
-const db_config = require("./config/db.config")
-const bcrypt = require("bcryptjs")
-const user_model = require("./models/user.model")
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const server_config = require("./config/serverConfig");
+const db_config = require("./config/db.config");
+const bcrypt = require("bcryptjs");
+const user_model = require("./models/user.model");
 /**
  * create an admin user at the starting of the application
  * if it is not already present
  */
 
 //connection with Mongodb
-mongoose.connect(db_config.DB_URL)
-const db = mongoose.connection
-db.on("error",()=>{
-        console.log("error while connecting")
-})
+mongoose.connect(db_config.DB_URL);
+const db = mongoose.connection;
 
-db.once("open",()=>{
-        console.log("connected")
-        init()
-})
+console.log(db);
+db.on("error", () => {
+  console.log("error while connecting");
+});
 
-async function init(){
-        let user = await user_model.findOne({userId : "admin"})
-        if(user){
-                console.log("Admin is already present",user)
-                return
+db.once("open", () => {
+  console.log("connected");
+  init();
+});
+
+async function init() {
+        let user = await user_model.findOne({ userId: "admin" });
+        if (user) {
+          console.log("Admin user already exists");
         }
-        try{
-                user = await user_model.create({
-                        name: "Arun Kumar",
-                        userId: "admin",
-                        email: "jellysabha@gmail.com",
-                        userType: "COUSTMER",
-                        password: bcrypt.hashSync("Hellllo5",9)
-                })
-                console.log("admin is created",user)
-        }catch(err){
-                console.log("error while create admin",err)
+      
+        try {
+          let newUser = new user_model({
+            name: "Piyush Bhatt",
+            userId: "thepiyush",
+            password: "12345",
+            email: "bhatt@gmail.com",
+            usertype: "COUSTMER",
+          });
+      
+          await newUser.save();
+          console.log("New user created successfully");
+        } catch (err) {
+            console.log("Error while saving new user:", err);
         }
-}
+      }
+      
+
 /**
  * start the server
  */
-app.listen(server_config.PORT,()=>{
-        console.log("server is starting at ",server_config.PORT)
-})
-
+app.listen(server_config.PORT, () => {
+  console.log("server is starting at ", server_config.PORT);
+});
